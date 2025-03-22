711d6c3e01e1dd211f6d1fe4001e9ac536f5dca9
Daria Makivska <peony.bluet@gmail.com>
2025-03-23 01:51:06 +0200

    Додає form в ДЗ №9

---
+++ src/js/2-form.js
@@ -0,0 +1,54 @@
+/* <form class="feedback-form" autocomplete="off">
+  <label>
+    Email
+    <input type="email" name="email" autofocus />
+  </label>
+  <label>
+    Message
+    <textarea name="message" rows="8"></textarea>
+  </label>
+  <button class="feedback-form-form" type="submit">
+    Submit
+  </button>
+</form>; */
+
+const formData = {
+  email: '',
+  message: '',
+};
+
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', handleInput);
+
+const STORAGE_KEY = 'feedback-form-state';
+
+function handleInput(event) {
+  formData[event.target.name] = event.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+const saveData = localStorage.getItem(STORAGE_KEY);
+
+if (saveData) {
+  const parseData = JSON.parse(saveData);
+
+  formData.email = parseData.email;
+  formData.message = parseData.message;
+
+  form.elements['email'].value = formData.email;
+  form.elements['message'].value = formData.message;
+}
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  if (!formData.email || !formData.message) {
+    alert('Fill please all fields');
+  } else {
+    console.log(formData);
+  }
+  event.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
